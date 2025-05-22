13ebf39d90819ad6957ebb4b8eee166b6933a384
strawgate <williamseaston@gmail.com>
2025-05-21 21:52:35 -0500

    Finishing work on filesystem operations mcp

---
+++ filesystem-operations-mcp/src/filesystem_operations_mcp/filesystem/server.py
@@ -0,0 +1,350 @@
+import re
+from fnmatch import fnmatch
+from pathlib import Path
+from typing import TypeVar
+
+from filesystem_operations_mcp.filesystem.models import (
+    FileEntry,
+    FileEntryChunk,
+    FileEntryChunkedContent,
+    FileEntryContent,
+    FileEntryPreview,
+    FlatDirectoryResult,
+    SummaryDirectoryEntry,
+)
+
+R = TypeVar("R", bound=FileEntry | FileEntryContent | FileEntryPreview)
+
+
+class FilesystemServerError(Exception):
+    """A base exception for the FilesystemServer."""
+
+    msg: str
+
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self) -> str:
+        return self.msg
+
+
+class FilesystemServerOutsideRootError(FilesystemServerError):
+    """An exception for when a path is outside the permitted root."""
+
+    def __init__(self, path: Path, root: Path):
+        super().__init__(f"Path {path} is outside the permitted root {root}")
+
+
+class FilesystemServerFileNotFoundError(FilesystemServerError):
+    """An exception for when a file does not exist."""
+
+    def __init__(self, path: Path):
+        super().__init__(f"File {path} does not exist")
+
+
+class FilesystemServer:
+    """The main filesystem server for the FilesystemOperationsMCP."""
+
+    root: Path
+
+    def __init__(self, root: Path):
+        self.root = root
+
+    @classmethod
+    def _resolve_path(cls, path: Path) -> Path:
+        """Resolve a path to an absolute path."""
+        if path.is_absolute():
+            return path
+
+        return path.resolve()
+
+    def _resolve_and_validate(self, path: Path) -> Path:
+        """Resolve a path to an absolute path and check if it is within the root."""
+        resolved_path = self._resolve_path(path)
+
+        try:
+            resolved_path.relative_to(self.root.resolve())
+        except ValueError as e:
+            raise FilesystemServerOutsideRootError(resolved_path, self.root) from e
+
+        return resolved_path
+
+    def _resolve_and_validate_list(self, paths: list[Path]) -> list[Path]:
+        """Resolve a list of paths to an absolute path and check if they are within the root."""
+        return [self._resolve_and_validate(path) for path in paths]
+
+    def _get_relative_path(self, path: Path) -> Path:
+        """Get the relative path to the root."""
+        return path.relative_to(self.root)
+
+    # File Commands
+
+    def file_exists(self, path: Path) -> bool:
+        """Check if a file exists."""
+        self._resolve_and_validate(path)
+
+        return path.is_file()
+
+    def delete_file(self, path: Path) -> None:
+        """Delete a file."""
+        self._resolve_and_validate(path)
+
+        if not self.file_exists(path):
+            raise FilesystemServerFileNotFoundError(path)
+
+        path.unlink()
+
+    def search_file(
+        self, path: Path, search: str, search_is_regex: bool = False, before: int = 3, after: int = 3
+    ) -> FileEntryChunkedContent | None:
+        """Search a file for a string or regex pattern."""
+        self._resolve_and_validate(path)
+
+        return self._find_in_file(
+            file=self._get_file(FileEntry, path), search=search, search_is_regex=search_is_regex, before=before, after=after
+        )
+
+    def create_file(self, path: Path, content: str) -> None:
+        """Create a file."""
+        self._resolve_and_validate(path)
+
+        path.write_text(content)
+
+    def append_file(self, path: Path, content: str) -> None:
+        """Append to a file."""
+        self._resolve_and_validate(path)
+
+        current_content = path.read_text()
+        path.write_text(current_content + content)
+
+    def read_file(self, path: Path) -> str:
+        """Read a file."""
+        return self._get_file(FileEntryContent, path).content
+
+    def preview_file(self, path: Path) -> str:
+        """Preview a file."""
+        result = self._get_file(FileEntryPreview, path)
+
+        if result.preview:
+            return result.preview
+
+        return result.content
+
+    def _get_file(self, entry_type: type[R], path: Path) -> R:
+        """Get a file."""
+        self._resolve_and_validate(path)
+
+        return entry_type(absolute_path=path, root=self.root)
+
+    # Directory Commands
+
+    def dir_exists(self, path: Path) -> bool:
+        """Check if a directory exists."""
+        self._resolve_and_validate(path)
+
+        return path.is_dir()
+
+    def get_dir(self, path: Path) -> FlatDirectoryResult[FileEntry]:
+        """Get a directory."""
+        self._resolve_and_validate(path)
+
+        return self.flat_dir(FileEntry, [path], recurse=False)
+
+    @classmethod
+    def _find_in_file(
+        cls, file: FileEntry, search: str, search_is_regex: bool = False, before: int = 3, after: int = 3
+    ) -> FileEntryChunkedContent | None:
+        """Find a regex in a file."""
+        chunks: list[FileEntryChunk] = []
+
+        file_content = file.read_lines()
+
+        for line_number, line in enumerate(file_content, start=1):
+            before_lines: list[str] = []
+            after_lines: list[str] = []
+            chunk: FileEntryChunk | None = None
+
+            if (search_is_regex and re.search(search, line)) or (not search_is_regex and search in line):
+                before_lines = file_content[max(0, line_number - before - 1) : max(0, line_number - 1)]
+                after_lines = file_content[min(len(file_content), line_number) : min(len(file_content), line_number + after)]
+
+                before_lines = [line.strip() for line in before_lines]
+                after_lines = [line.strip() for line in after_lines]
+
+                chunk = FileEntryChunk(before=before_lines, match=line, after=after_lines, match_line_number=line_number)
+                chunks.append(chunk)
+
+        return FileEntryChunkedContent(absolute_path=file.absolute_path, root=file.root, chunks=chunks) if chunks else None
+
+    def search_content(
+        self,
+        path: list[Path],
+        search: str,
+        recurse: bool,
+        search_is_regex: bool = False,
+        include: list[str] | None = None,
+        exclude: list[str] | None = None,
+        before: int = 3,
+        after: int = 3,
+    ) -> FlatDirectoryResult[FileEntryChunkedContent]:
+        """Search the files in a directory for text or a regex pattern. Will not search files larger than 5mb.
+
+        Args:
+            path: The path to search.
+            recurse: Whether to recurse into subdirectories.
+            search: The string to search for.
+            search_is_regex: Whether the search is a regex.
+            include: A list of patterns to include.
+            exclude: A list of patterns to exclude.
+            before: The number of lines to include before the match.
+            after: The number of lines to include after the match.
+
+        Returns:
+            A list of FileEntryChunkedContent objects.
+
+        Example:
+            >>> fs = FilesystemServer(Path("/home/user"))
+            >>> fs.search_dir([Path("documents")], recurse=True, search="hello", is_regex=False, include=["*.txt"], exclude=["*.md"], before=3, after=3)
+            [FileEntryChunkedContent(chunks=[FileEntryChunk(before=["This is a test"], match="hello", after=["This is a test"], match_line_number=1)])]
+        """  # noqa: E501 Ignore long line length
+        self._resolve_and_validate_list(path)
+
+        flat_directory_result = self.flat_dir(FileEntry, path, recurse=recurse, include=include, exclude=exclude)
+        new_flat_directory_result = FlatDirectoryResult[FileEntryChunkedContent]({})
+        results: list[FileEntryChunkedContent] = []
+
+        for dir_name, directory in flat_directory_result.root.items():
+            for file_name, file in directory.items():
+                if isinstance(file, SummaryDirectoryEntry) or file.size() > 5 * 1024 * 1024:
+                    continue
+
+                result = self._find_in_file(file=file, search=search, search_is_regex=search_is_regex, before=before, after=after)
+                if result:
+                    results.append(result)
+
+                    if dir_name not in new_flat_directory_result.root:
+                        new_flat_directory_result.root[dir_name] = {}
+
+                    new_flat_directory_result.root[dir_name][file_name] = result
+
+        return new_flat_directory_result
+
+    def flat_dir(
+        self,
+        entry_type: type[R],
+        path: list[Path] | None = None,
+        recurse: bool = False,
+        include: list[str] | None = None,
+        exclude: list[str] | None = None,
+    ) -> FlatDirectoryResult[R]:
+        """Get an LLM-friendly flat directory result.
+
+        Args:
+            entry_type: The type of entry to return.
+            path: The path to the directory to get.
+            recurse: Whether to recurse into subdirectories.
+            include: A list of patterns to include.
+            exclude: A list of patterns to exclude.
+
+        Returns:
+            A flat directory result.
+
+        Example:
+            >>> fs = FilesystemServer(Path("/home/user"))
+            >>> fs.flat_dir(FileEntry, [Path("documents")], recurse=True)
+            FlatDirectoryResult(
+                root={
+                    "documents": {
+                        "file1.txt": FileEntry(absolute_path=Path("/home/user/documents/file1.txt"), root=Path("/home/user")),
+                    }
+                }
+            )
+        """
+        if path is None:
+            path = [Path()]
+
+        path = self._resolve_and_validate_list(path)
+
+        flat_directory_result = FlatDirectoryResult[entry_type]({})
+
+        walk = self._filtered_walk(path, recurse, include, exclude)
+
+        files: set[Path] = set()
+        directories: set[Path] = set()
+
+        for child in walk:
+            entry = self.root / child
+            if entry.is_file():
+                files.add(child)
+                directories.add(child.parent)
+            elif entry.is_dir():
+                directories.add(child)
+
+        # Now we render our flat directory result
+        for directory in directories:
+            # We want to use the relative path to the root for the keys in the flat directory result
+            relative_directory = str(self._get_relative_path(directory))
+            if relative_directory != ".":
+                relative_directory = "./" + relative_directory
+
+            # now we get all of the files that are in this directory from files
+            files_in_dir = [child for child in files if child.parent == directory]
+
+            # now we add the files to the flat directory result
+
+            children = list(directory.glob("*"))
+
+            entry = flat_directory_result.root.setdefault(relative_directory, {})
+
+            entry.update(
+                **{".": SummaryDirectoryEntry(absolute_path=directory, root=self.root, children=len(children))},
+                **{child.name: entry_type(absolute_path=child, root=self.root) for child in files_in_dir},
+            )
+
+        return flat_directory_result
+
+    def _filtered_walk(
+        self, path: list[Path], recurse: bool, include: list[str] | None = None, exclude: list[str] | None = None
+    ) -> list[Path]:
+        """Walk a directory and filter the entries.
+
+        Args:
+            path: The path to the directory to walk.
+            recurse: Whether to recurse into subdirectories.
+            include: A list of patterns to include.
+            exclude: A list of patterns to exclude.
+
+        Returns:
+            A list of paths.
+
+        Example:
+            >>> fs = FilesystemServer(Path("/home/user"))
+            >>> fs._filtered_walk([Path("documents")], recurse=True, include=["*.txt"], exclude=["*.md"])
+            [Path("/home/user/documents/file1.txt"), Path("/home/user/documents/file2.txt")]
+
+            >>> fs._filtered_walk([Path("documents")], recurse=True, include=["*.txt"], exclude=["*.md"])
+            [Path("/home/user/documents/file1.txt"), Path("/home/user/documents/file2.txt")]
+        """
+        entries = set()
+
+        if not include:
+            include = ["*"]
+
+        if not exclude:
+            exclude = []
+
+        for p in path:
+            joined_path = self.root / p
+            self._resolve_and_validate(joined_path)
+
+            glob_function = joined_path.rglob if recurse else joined_path.glob
+
+            for this_include in include:
+                entries.update(list(glob_function(this_include)))
+
+        entries = list(entries)
+
+        if exclude:
+            entries = [e for e in entries if not any(fnmatch(e, this_exclude) for this_exclude in exclude)]
+
+        return entries
---
+++ filesystem-operations-mcp/src/filesystem_operations_mcp/main.py
@@ -0,0 +1,370 @@
+import asyncio
+from pathlib import Path
+from typing import Any
+
+import asyncclick as click
+import pydantic_core
+import yaml
+from fastmcp import Context, FastMCP
+from fastmcp.contrib.mcp_mixin.mcp_mixin import MCPMixin, mcp_tool
+from pydantic import BaseModel
+
+from filesystem_operations_mcp.filesystem.models import (
+    FileEntry,
+    FileEntryChunkedContent,
+    FileEntryContent,
+    FileEntryPreview,
+    FlatDirectoryResult,
+)
+from filesystem_operations_mcp.filesystem.server import FilesystemServer
+from filesystem_operations_mcp.filesystem.skip import DEFAULT_SKIP_LIST, DEFAULT_SKIP_READ
+
+
+class FileServer(MCPMixin):
+    def __init__(self, filesystem_server: FilesystemServer):
+        self.filesystem_server = filesystem_server
+
+    @mcp_tool()
+    def read(self, ctx: Context, path: Path) -> str:
+        """Read the contents of a file.
+
+        Args:
+            path: The path to the file to read.
+
+        Returns:
+            The contents of the file.
+        """
+        ctx.info(f"Request to read file {path}")
+        return self.filesystem_server.read_file(path)
+
+    @mcp_tool()
+    def preview(self, ctx: Context, path: Path) -> str:
+        """Preview the contents of a file.
+
+        Args:
+            path: The path to the file to preview.
+
+        Returns:
+            A preview of the contents of the file.
+        """
+        ctx.info(f"Request to preview file {path}")
+        return self.filesystem_server.preview_file(path)
+
+    @mcp_tool()
+    def delete(self, ctx: Context, path: Path) -> bool:
+        """Delete a file.
+
+        Args:
+            path: The path to the file to delete.
+
+        Returns:
+            True if the file was deleted. Raises an error otherwise.
+        """
+        ctx.info(f"Request to delete file {path}")
+        self.filesystem_server.delete_file(path)
+        return True
+
+    @mcp_tool()
+    def search(
+        self, ctx: Context, path: Path, search: str, search_is_regex: bool = False, before: int = 3, after: int = 3
+    ) -> FileEntryChunkedContent | None:
+        """Search a specific file for a string or regex pattern.
+
+        Args:
+            path: The path to the file to search.
+            search: The search string.
+            search_is_regex: Whether the search is a regex.
+            before: The number of lines to include before the match.
+            after: The number of lines to include after the match.
+
+        Returns:
+            A list of chunks containing the search results.
+
+        Example:
+            >>> search(path="a.txt", search="hello", search_is_regex=False, before=3, after=3)
+            [
+                FileEntryChunkedContent(path="a.txt", content="hello world"),
+            ]
+        """
+        ctx.info(f"Request to search file {path} for {search}")
+        return self.filesystem_server.search_file(path=path, search=search, search_is_regex=search_is_regex, before=before, after=after)
+
+    @mcp_tool()
+    def create(self, ctx: Context, path: Path, content: str) -> bool:
+        """Create a file.
+
+        Args:
+            path: The path to the file to create.
+            content: The content to write to the file.
+
+        Returns:
+            True if the file was created. Raises an error otherwise.
+        """
+        ctx.info(f"Request to create file {path}")
+
+        self.filesystem_server.create_file(path, content)
+
+        return True
+
+    @mcp_tool()
+    def append(self, ctx: Context, path: Path, content: str) -> bool:
+        """Append to a file.
+
+        Args:
+            path: The path to the file to append to.
+            content: The content to append to the file.
+
+        Returns:
+            True if the file was appended to. Raises an error otherwise.
+        """
+
+        ctx.info(f"Request to append to file {path}")
+
+        self.filesystem_server.append_file(path, content)
+
+        return True
+
+
+class DirectoryServer(MCPMixin):
+    def __init__(self, filesystem_server: FilesystemServer):
+        self.filesystem_server = filesystem_server
+
+    @mcp_tool(name="list")
+    def list_contents(
+        self,
+        ctx: Context,
+        path: list[Path],
+        include: list[str] | None = None,
+        exclude: list[str] | None = None,
+        recurse: bool = False,
+        bypass_default_exclusions: bool = False,
+    ) -> FlatDirectoryResult[FileEntry]:
+        """List the contents of a directory.
+
+        Args:
+            path: (Required) A list of paths to list contents for.
+            recurse: (Optional) Whether to recurse into subdirectories.
+            include: (Optional) A list of glob patterns to include.
+            exclude: (Optional) A list of glob patterns to exclude.
+            bypass_default_exclusions: (Optional) Whether to bypass the default exclusions. By default, hidden folders
+                and cache directories are excluded. Defaults to False.
+
+        Returns:
+            A flat directory result containing the contents of listed directories.
+
+        Example:
+            >>> list(recurse=True, include=["*.txt"], exclude=["*.md"])
+            {
+                ".": {
+                    "a.txt": FileEntry(path="a.txt", size=100),
+                    "b.txt": FileEntry(path="b.txt", size=100),
+                },
+                "subdir": {
+                    "c.txt": FileEntry(path="subdir/c.txt", size=100),
+                },
+            }
+        """
+
+        if not bypass_default_exclusions:
+            exclude = exclude or []
+            exclude.extend(DEFAULT_SKIP_LIST)
+
+        ctx.info(f"Request to list contents of {path}: recurse={recurse}, include={include}, exclude={exclude}")
+        return self.filesystem_server.flat_dir(FileEntry, path, recurse, include, exclude)
+
+    @mcp_tool(name="preview")
+    def preview_contents(
+        self,
+        ctx: Context,
+        path: list[Path],
+        include: list[str],
+        exclude: list[str],
+        recurse: bool = False,
+        bypass_default_exclusions: bool = False,
+    ) -> FlatDirectoryResult[FileEntryPreview]:
+        """Preview the contents of the files in a directory.
+
+        Args:
+            path: (Required) A list of paths to preview contents for.
+            recurse: (Required) Whether to recurse into subdirectories.
+            include: (Required) A list of glob patterns to include.
+            exclude: (Required) A list of glob patterns to exclude.
+            bypass_default_exclusions: (Optional) Whether to bypass the default exclusions. By default, hidden folders, cache directories,
+             and compiled files are excluded. Defaults to False.
+
+        Returns:
+            A flat directory result containing the previewed contents of listed directories. If the file is smaller than the preview
+            size, the preview will be placed in the contents field. A preview will always end in `...`
+
+        Example:
+            >>> preview(recurse=True, include=["*.txt"], exclude=["*.md"])
+            {
+                ".": {
+                    "a.txt": FileEntryPreview(path="a.txt", size=11, content="hello world"),
+                },
+                "subdir": {
+                    "c.txt": FileEntryPreview(path="subdir/c.txt", size=1000, preview="hello world how are you doing today..."),
+                },
+            }
+        """
+        if not bypass_default_exclusions:
+            exclude = exclude or []
+            exclude.extend(DEFAULT_SKIP_READ)
+            exclude.extend(DEFAULT_SKIP_LIST)
+
+        ctx.info(f"Request to preview contents of {path}: recurse={recurse}, include={include}, exclude={exclude}")
+        return self.filesystem_server.flat_dir(FileEntryPreview, path, recurse, include, exclude)
+
+    @mcp_tool(name="read")
+    def read_contents(
+        self,
+        ctx: Context,
+        path: list[Path],
+        include: list[str],
+        exclude: list[str],
+        recurse: bool = False,
+        bypass_default_exclusions: bool = False,
+    ) -> FlatDirectoryResult[FileEntryContent]:
+        """Read the contents of the files in a directory.
+
+        Args:
+            path: (Required) A list of paths to read contents for.
+            recurse: (Required) Whether to recurse into subdirectories.
+            include: (Required) A list of glob patterns to include.
+            exclude: (Required) A list of glob patterns to exclude.
+            bypass_default_exclusions: (Optional) Whether to bypass the default exclusions. By default, hidden folders, cache directories,
+             and compiled files are excluded.
+
+        Returns:
+            A flat directory result containing the read contents of listed directories.
+
+        Example:
+            >>> read_all_contents(recurse=True, include=["*.txt"], exclude=["*.md"])
+            {
+                ".": {
+                    "a.txt": FileEntryContent(path="a.txt", content="hello world"),
+                },
+            }
+        """
+
+        if not bypass_default_exclusions:
+            exclude = exclude or []
+            exclude.extend(DEFAULT_SKIP_READ)
+            exclude.extend(DEFAULT_SKIP_LIST)
+
+        ctx.info(f"Request to read contents of {path}: recurse={recurse}, include={include}, exclude={exclude}")
+        return self.filesystem_server.flat_dir(FileEntryContent, path, recurse, include, exclude)
+
+    @mcp_tool(name="search")
+    def search_contents(
+        self,
+        ctx: Context,
+        path: list[Path],
+        search: str,
+        recurse: bool = False,
+        include: list[str] | None = None,
+        exclude: list[str] | None = None,
+        before: int = 3,
+        after: int = 3,
+        search_is_regex: bool = False,
+        bypass_default_exclusions: bool = False,
+    ) -> FlatDirectoryResult[FileEntryChunkedContent]:
+        """Search the contents of the files in a directory.
+
+        Args:
+            path: (Required) A list of directories to search files for contents.
+            search: (Required) The search string.
+            recurse: (Optional) Whether to recurse into subdirectories.
+            include: (Optional) A list of glob patterns to include.
+            exclude: (Optional) A list of glob patterns to exclude.
+            before: (Optional) The number of lines to include before the match.
+            after: (Optional) The number of lines to include after the match.
+            search_is_regex: (Optional) Whether the search is a regex.
+            bypass_default_exclusions: (Optional) Whether to bypass the default exclusions. By default, hidden folders, cache directories,
+             and compiled files are excluded.
+
+        Returns:
+            A flat directory result containing the search results.
+
+        Example:
+            >>> search(recurse=True, include=["*.txt"], exclude=["*.md"], before=3, after=3, search_is_regex=False)
+            {
+                ".": {
+                    "a.txt": FileEntryChunkedContent(path="a.txt", content="hello world"),
+                },
+            }
+        """
+        if not bypass_default_exclusions:
+            exclude = exclude or []
+            exclude.extend(DEFAULT_SKIP_READ)
+            exclude.extend(DEFAULT_SKIP_LIST)
+
+        ctx.info(
+            f"Request to search contents of {path}: recurse={recurse}, include={include}, exclude={exclude}, before={before}, after={after}, search_is_regex={search_is_regex}"  # noqa: E501
+        )
+        return self.filesystem_server.search_content(
+            path=path,
+            search=search,
+            recurse=recurse,
+            include=include,
+            exclude=exclude,
+            before=before,
+            after=after,
+            search_is_regex=search_is_regex,
+        )
+
+
+ROOT_DIR_HELP = "The allowed filesystem paths for filesystem operations. Defaults to the current working directory for the server."
+MAX_SIZE_HELP = "The maximum size of a result in bytes before throwing an exception. Defaults to 400 kb or about 100k tokens."
+SERIALIZE_AS_HELP = "The format to serialize the response in. Defaults to Yaml"
+MCP_TRANSPORT_HELP = "The transport to use for the MCP server. Defaults to stdio."
+
+
+@click.command()
+@click.option("--root-dir", type=str, default=Path.cwd(), help=ROOT_DIR_HELP)
+@click.option("--max-size", type=int, default=400_000, help=MAX_SIZE_HELP)
+@click.option("--serialize-as", type=click.Choice(["json", "yaml"]), default="json", help=SERIALIZE_AS_HELP)
+@click.option("--mcp-transport", type=click.Choice(["stdio", "sse", "streamable-http"]), default="stdio", help=MCP_TRANSPORT_HELP)
+async def cli(root_dir: str, max_size: int, serialize_as: str, mcp_transport: str):
+    def yaml_serializer(data: Any) -> str:
+        if isinstance(data, BaseModel):
+            data = pydantic_core.to_jsonable_python(data, fallback=str, exclude_none=True)
+
+        result = yaml.safe_dump(data, width=300, sort_keys=False, default_flow_style=False)
+
+        if len(result) > max_size:
+            msg = f"Result is too large {len(result)} bytes. Max size is {max_size} bytes. Use more specific filters to return less data."
+            raise ValueError(msg)
+
+        return result
+
+    def json_serializer(data: Any) -> str:
+        result = pydantic_core.to_json(data, fallback=str, indent=2, exclude_none=True).decode()
+
+        if len(result) > max_size:
+            msg = f"Result is too large {len(result)} bytes. Max size is {max_size} bytes. Use more specific filters to return less data."
+            raise ValueError(msg)
+
+        return result
+
+    serializer = yaml_serializer if serialize_as == "yaml" else json_serializer
+
+    mcp = FastMCP(name="Local Filesystem Operations MCP", tool_serializer=serializer)
+
+    server = FilesystemServer(Path(root_dir))
+
+    directory_server = DirectoryServer(server)
+    directory_server.register_tools(mcp, prefix="directory")
+
+    file_server = FileServer(server)
+    file_server.register_tools(mcp, prefix="file")
+
+    await mcp.run_async(transport=mcp_transport)
+
+
+def run_mcp():
+    asyncio.run(cli())
+
+
+if __name__ == "__main__":
+    run_mcp()
